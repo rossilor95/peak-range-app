f22faa2d5fbecf3d24bc90a0afcaf9887db2667c
rossilor95 <rossilor95@outlook.it>
2023-08-03 11:13:39 +0200

    first commit

---
+++ src/main/java/com/github/rossilor95/eventspeakfinder/EventsPeakFinder.java
@@ -0,0 +1,7 @@
+package com.github.rossilor95.eventspeakfinder;
+
+public class EventsPeakFinder {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
