b6f6978dbdf789758a8ecd2c064fbd24d004c475
rossilor95 <rossilor95@outlook.it>
2023-08-05 16:58:50 +0200

    feat: add new enum EndpointType

---
+++ src/main/java/com/github/rossilor95/eventspeakfinder/EndpointType.java
@@ -0,0 +1,32 @@
+package com.github.rossilor95.eventspeakfinder;
+
+/**
+ * Enum representing the type of interval endpoint.
+ */
+enum EndpointType {
+
+    /**
+     * Represents the start of a time interval.
+     */
+    START('S'),
+
+    /**
+     * Represents the end of a time interval.
+     */
+    END('E');
+
+    private final char flag;
+
+    EndpointType(char flag) {
+        this.flag = flag;
+    }
+
+    /**
+     * Get the character flag associated with the endpoint type.
+     *
+     * @return The character flag
+     */
+    public char getFlag() {
+        return flag;
+    }
+}
