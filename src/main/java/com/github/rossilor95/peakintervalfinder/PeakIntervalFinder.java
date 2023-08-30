0c9a6dfc4120e92dfd98b052a0a240b2473d279b
rossilor95 <rossilor95@outlook.it>
2023-08-30 18:21:22 +0200

    refactor: make PeakIntervalFinder instantiable

--- src/main/java/com/github/rossilor95/peakintervalfinder/PeakIntervalFinder.java
+++ src/main/java/com/github/rossilor95/peakintervalfinder/PeakIntervalFinder.java
@@ -2,10 +2,15 @@ package com.github.rossilor95.peakintervalfinder;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PeakIntervalFinder {
+    private final TimeIntervalDataProcessor timeIntervalDataProcessor;
+
+    PeakIntervalFinder(TimeIntervalDataProcessor timeIntervalDataProcessor) {
+        this.timeIntervalDataProcessor = timeIntervalDataProcessor;
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.exit(-1);
@@ -13,19 +18,21 @@ public class PeakIntervalFinder {
         String filePath = args[0];
 
         final TimeIntervalDataProcessor timeIntervalDataProcessor = new TimeIntervalDataProcessor();
-        List<IntervalEndpoint> endpoints = timeIntervalDataProcessor.processDataFile(filePath);
-
-        int[] eventCount = findEventCount(endpoints);
-        System.out.println(Arrays.toString(eventCount));
-
-        int maxEventCount = findMax(eventCount);
-        List<Integer> maxIndices = findMaxIndices(eventCount, maxEventCount);
-        List<TimeInterval> peakIntervals = findPeakIntervals(endpoints, maxIndices);
+        final PeakIntervalFinder peakIntervalFinder = new PeakIntervalFinder(timeIntervalDataProcessor);
+        List<TimeInterval> peakIntervals = peakIntervalFinder.findPeakIntervals(filePath);
 
         System.out.println(peakIntervals);
     }
 
-    private static int[] findEventCount(List<IntervalEndpoint> endpoints) {
+    public List<TimeInterval> findPeakIntervals(String filePath) throws IOException {
+        List<IntervalEndpoint> endpoints = timeIntervalDataProcessor.processDataFile(filePath);
+        int[] eventCount = findEventCount(endpoints);
+        int maxEventCount = findMax(eventCount);
+        List<Integer> maxIndices = findMaxIndices(eventCount, maxEventCount);
+        return findPeakIntervals(endpoints, maxIndices);
+    }
+
+    private int[] findEventCount(List<IntervalEndpoint> endpoints) {
         int[] eventCount = new int[endpoints.size()];
         eventCount[0] = endpoints.get(0).type() == EndpointType.START ? 1 : 0;
         for (int i = 1; i < endpoints.size(); i++) {
@@ -35,17 +42,7 @@ public class PeakIntervalFinder {
         return eventCount;
     }
 
-    private static int findMax(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    private static List<Integer> findMaxIndices(int[] arr, int max) {
+    private List<Integer> findMaxIndices(int[] arr, int max) {
         List<Integer> maxIndices = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == max) {
@@ -55,12 +52,22 @@ public class PeakIntervalFinder {
         return maxIndices;
     }
 
-    private static List<TimeInterval> findPeakIntervals(List<IntervalEndpoint> endpoints, List<Integer> maxIndices) {
+    private List<TimeInterval> findPeakIntervals(List<IntervalEndpoint> endpoints, List<Integer> maxIndices) {
         List<TimeInterval> peakIntervals = new ArrayList<>();
         for (int index : maxIndices) {
             TimeInterval peakInterval = new TimeInterval(endpoints.get(index).time(), endpoints.get(index + 1).time());
             peakIntervals.add(peakInterval);
         }
         return peakIntervals;
+    }
+
+    private int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
