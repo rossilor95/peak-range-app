b69602e5e532264911ff2bd1c94c68c0edddae38
rossilor95 <rossilor95@outlook.it>
2023-12-20 11:56:32 +0100

    build: update gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    id("java")
+    application
 }
 
 group = "com.github.rossilor95"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -14,6 +15,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.mockito:mockito-core:5.5.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.5.0")
+}
+
+application {
+    mainClass.set("com.github.rossilor95.peakintervalfinder.PeakIntervalFinder")
 }
 
 tasks.test {
