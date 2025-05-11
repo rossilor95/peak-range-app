dea19a0eeabb86b634ff0eb97b606c206c7a6f36
rossilor95 <rossilor95@outlook.it>
2025-05-11 12:09:05 +0200

    build: update Gradle to v8.14 and dependencies for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,28 @@ plugins {
     application
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(24))
+    }
+}
+
 group = "com.github.rossilor95"
 version = "1.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
 }
 
+val jUnitVersion = "5.12.2"
+val mockitoVersion = "5.17.0"
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.11.1"))
+    testImplementation(platform("org.junit:junit-bom:$jUnitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito:mockito-core:5.11.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.14.0")
 }
 
 application {
